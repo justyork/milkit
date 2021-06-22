5c9d2e5853512cc647686fe59a583bdc59bc240c
york <yorkshp@gmail.com>
2021-06-22 15:25:07 +0300

    init

---
+++ background.js
@@ -0,0 +1,30 @@
+const WS_ENDPOINT = 'wss://ext-ws-server-dev.milkit.workers.dev/ws';
+
+class App {
+    run (userId) {
+        this.connectToWs(userId)
+    }
+
+    connectToWs(userId) {
+        const websocket = new WebSocket(`${WS_ENDPOINT}?user_id=${userId}`)
+
+        websocket.addEventListener('message', event => {
+            console.log('Message received from server')
+            console.log(event.data)
+        })
+
+        websocket.onopen = function(e) {
+            console.log('[open] Connection established')
+            console.log('Sending to server')
+            websocket.send(JSON.stringify({ message: 'My name is John' }))
+        }
+
+    }
+
+}
+(new App).run(USER_ID);
+
+
+runtime.onInstalled.addListener(async (details) => {
+    console.log('install', details);
+});
